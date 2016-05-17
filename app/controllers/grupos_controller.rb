e59c6316b06c6a2ccd4592bb3bf9e0ff14e9d488
Juan Carlos Santiago <krlos013@gmail.com>
2016-05-17 15:00:27 -0500

    8

--- app/controllers/grupos_controller.rb
+++ app/controllers/grupos_controller.rb
@@ -25,17 +25,17 @@ class GruposController < ApplicationController
   # POST /grupos.json
   def create
     @grupo = Grupo.new(grupo_params)
-
+    logger.debug grupo_params[:nombre]
     respond_to do |format|
       if @grupo.save
   RestClient.post "https://api:key-e7d79c66e74391fdf48b657624f23ddc"\
   "@api.mailgun.net/v3/sandboxb9c2dadab0ea49f6b7130d1091646c59.mailgun.org/messages",
-  :from => "Worbe <mailgun@sandboxb9c2dadab0ea49f6b7130d1091646c59.mailgun.org>",
-  :to => params[:email],
-  :subject => "Curriculum Compartido",
-  :html => "El usuario #{current_user.id} solicita la creación de un nuevo directorio, acontinuación los detalles:"\
-           "Nombre: #{nombre}"\
-           "Descripcion: #{descripcion}"
+  :from => "Bikina <no-reply@bikina.mx>",
+  :to => "krlos013@gmail.com",
+  :subject => "Solicitud de creación de directorio",
+  :html => "El usuario #{current_user.email} solicita la creación de un nuevo directorio, a continuación los detalles:"\
+           "<p>Nombre: #{grupo_params[:nombre]}, clave: #{grupo_params[:clave]}</p>"\
+           "<p>Descripción: #{grupo_params[:descripcion]} </p>"
         format.html { redirect_to '/', notice: 'Grupo was successfully created.' }
         format.json { render :show, status: :created, location: @grupo }
       else
@@ -74,6 +74,8 @@ class GruposController < ApplicationController
     def set_grupo
       @grupo = Grupo.find(params[:id])
     end
+
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def grupo_params
