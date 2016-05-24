f43367a0d087c02d276c424871fed079fff3a267
Juan Carlos Santiago <krlos013@gmail.com>
2016-05-23 20:55:53 -0500

    11

--- app/controllers/api/v1/directorios_random_controller.rb
+++ app/controllers/api/v1/directorios_random_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::DirectoriosRandomController <ApplicationController
 
 		@unir = Unir.where(:user_id => current_user.id).pluck(:grupo_id)
 		@cp = Perfil.where(:user_id => current_user.id).pluck(:cp)
-		directorios = Grupo.select(:nombre, :clave).where(:publicado => "1").where.not(:clave => @unir).where(:cp => @cp).order("RANDOM()").limit(20)
+		directorios = Grupo.select(:nombre, :clave,:tipo).where(:publicado => "1").where.not(:clave => @unir).where(:cp => @cp).order("RANDOM()").limit(20)
 		render json: directorios
 		
 	end
