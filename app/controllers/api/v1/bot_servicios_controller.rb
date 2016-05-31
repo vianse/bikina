c86ff3227824865e5bd10eb16d5fc3a968ede8ff
Juan Carlos Santiago <krlos013@gmail.com>
2016-05-31 15:02:39 -0500

    16

--- app/controllers/api/v1/bot_servicios_controller.rb
+++ app/controllers/api/v1/bot_servicios_controller.rb
@@ -1,7 +1,64 @@
 class Api::V1::BotServiciosController <ApplicationController
 	def index
-		render json: '{"attachment":{"type":"template","payload":{"template_type":"generic","elements":[{"title":"Persistir\u00e1n lluvias en territorio salvadore\u00f1o","subtitle":"Pronostican que por la noche habr\u00e1&#160;chubascos y tormentas aisladas\n","image_url":"http:\/\/am-elsalv-cdn.agilecontents.com\/resources\/jpg\/6\/0\/1464698432006.jpg","buttons":[{"type":"web_url","url":"http:\/\/www.elsalvador.com\/\/articulo\/comunidades\/persistiran-lluvias-territorio-salvadoreno-114314","title":"Leer Noticia"},{"type":"postback","title":"Otra Busqueda","payload":"edhbusqueda"}]},{"title":"Los \u00e1rboles y su funci\u00f3n en la ecolog\u00eda","subtitle":"Se reduce el calor y se protegen mantos acu\u00edferos. Dos razones suficientes para tomar en serio la importancia de reforestar.\n","image_url":"http:\/\/am-elsalv-cdn.agilecontents.com\/resources\/jpg\/9\/2\/1464649099929.jpg","buttons":[{"type":"web_url","url":"http:\/\/www.elsalvador.com\/\/articulo\/comunidades\/los-arboles-funcion-ecologia-114277","title":"Leer Noticia"},{"type":"postback","title":"Otra Busqueda","payload":"edhbusqueda"}]},{"title":"Pocos clientes llegan a la nueva plaza en Ahuachap\u00e1n","subtitle":"&#160;Alcalde Abilio Flores se limita a recomendar a las personas que compren en el lugar y los otros mercados. Algunos vendedores est\u00e1n regresando a las calles.\n","image_url":"http:\/\/am-elsalv-cdn.agilecontents.com\/resources\/jpg\/5\/5\/1464649099955.jpg","buttons":[{"type":"web_url","url":"http:\/\/www.elsalvador.com\/\/articulo\/comunidades\/pocos-clientes-llegan-nueva-plaza-ahuachapan-114276","title":"Leer Noticia"},{"type":"postback","title":"Otra Busqueda","payload":"edhbusqueda"}]}]}}}'
+		@servicios = Registro.select(:titulo, :descripcion,:user_id,:telefono1,:correo,:countclicks, :user_id,:id).where(:grupo_id => params[:clave]).order('id DESC')
+		
+		@estructura = '{"attachment":
+		{"type":"template","payload":
+			{"template_type":"generic", 
+				"elements":'
+		@estructurafinal = '}}}'
+		servicios = @servicios.map do |a|
+			{
+				:title => "Persistir\u00e1n lluvias en territorio salvadore\u00f1o",
+				:subtitle =>"Pronostican que por la noche habr\u00e1&#160;chubascos y tormentas aisladas\n",
+				:image_url => "http:\/\/am-elsalv-cdn.agilecontents.com\/resources\/jpg\/6\/0\/1464698432006.jpg",
+				:buttons => [{
+				 	            :type=>"postback",
+						   		:title=>"Otra Busqueda",
+						        :payload=>"edhbusqueda"}
+						      ]
+			}
+	    end
 
+	    render json: @estructura.to_s + servicios.to_json + @estructurafinal.to_s
+
+		# render json: '
+		# {
+		# 	 "attachment":
+		# 	 	{"type":"template","payload":
+		# 	 		{"template_type":"generic",
+		# 	 		 "elements":[{
+		# 	 		 			 "title":"Persistir\u00e1n lluvias en territorio salvadore\u00f1o",
+		# 						 "subtitle":"Pronostican que por la noche habr\u00e1&#160;chubascos y tormentas aisladas\n",
+		# 						 "image_url":"http:\/\/am-elsalv-cdn.agilecontents.com\/resources\/jpg\/6\/0\/1464698432006.jpg",
+		# 						 "buttons":[{"type":"web_url",
+		# 						 			 "url":"http:\/\/www.elsalvador.com\/\/articulo\/comunidades\/persistiran-lluvias-territorio-salvadoreno-114314",
+		# 				 					 "title":"Leer Noticia"
+		# 				 					 },
+		# 				 				     {
+		# 				 				     "type":"postback",
+		# 				   					 "title":"Otra Busqueda",
+		# 				                     "payload":"edhbusqueda"}
+		# 				                    ]},
+		# 	   					{
+		# 	   					 "title":"Los \u00e1rboles y su funci\u00f3n en la ecolog\u00eda",
+		# 	   					 "subtitle":"Se reduce el calor y se protegen mantos acu\u00edferos. Dos razones suficientes para tomar en serio la importancia de reforestar.\n",
+		# 	   					 "image_url":"http:\/\/am-elsalv-cdn.agilecontents.com\/resources\/jpg\/9\/2\/1464649099929.jpg",
+		# 	  					 "buttons":
+		# 	  					 		   [{
+		# 	  					 		   	  "type":"web_url",
+		# 	   								  "url":"http:\/\/www.elsalvador.com\/\/articulo\/comunidades\/los-arboles-funcion-ecologia-114277",
+		# 	   								  "title":"Leer Noticia"
+		# 	   								},
+		# 	   							    {
+		# 	   							      "type":"postback",
+		# 	   								  "title":"Otra Busqueda",
+		# 	   								  "payload":"edhbusqueda"
+		# 	   								 }
+		# 	   							   ]}
+		# 	  					]}}}
+
+		# '
 
 		# if params[:q].present?
 		# servicios = Registro.select(:titulo, :descripcion,:user_id,:telefono1,:correo,:countclicks, :user_id,:id).where("lower(descripcion) LIKE ?", "%#{params[:q.downcase]}%").where(:grupo_id => params[:clave]).paginate(:page => params[:page]).order('id DESC')
