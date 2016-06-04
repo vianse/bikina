e877e4cd87b74489abba0ad451a86f914b64f99d
Juan Carlos Santiago <krlos013@gmail.com>
2016-06-03 22:45:47 -0500

    17

--- app/controllers/api/v1/servicio_all_controller.rb
+++ app/controllers/api/v1/servicio_all_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ServicioAllController <ApplicationController
 	def index
-		servicios = Registro.select(:titulo, :descripcion,:user_id,:telefono1,:correo,:countclicks,:user_id,:id,:recomendado).where(:grupo_id => params[:clave]).paginate(:page => params[:page]).order('id DESC')
+		servicios = Registro.select(:titulo, :descripcion,:user_id,:telefono1,:correo,:countclicks,:user_id,:id,:recomendado).where(:grupo_id => params[:clave])
 		servicio = servicios.map do |a|
 			@url ="http://dummyimage.com/600x400/380f38/ffffff&text=b"
 			if a.recomendado=="si"
