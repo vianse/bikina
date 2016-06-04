fcb306d37e97a800b14a1c796932c82c66061c25
Juan Carlos Santiago <krlos013@gmail.com>
2016-06-03 22:12:12 -0500

    17

--- app/controllers/api/v1/directorios_controller.rb
+++ app/controllers/api/v1/directorios_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::DirectoriosController <ApplicationController
 	def index
 		if params[:q].present?
 		@unir = Unir.where(:user_id => current_user.id).pluck(:grupo_id)
-		directorios = Grupo.select(:nombre, :clave).where("lower(nombre) LIKE ?", "%#{params[:q.downcase]}%").where(:publicado => "1").where.not(:clave => @unir)
+
+		directorios = Grupo.select(:nombre, :clave).where("lower(nombre) LIKE ?", "%#{params[:q.downcase]}%").where(:publicado => "1").where(:cp => Perfil.where(:user_id => current_user.id).pluck(:cp).first).where.not(:clave => @unir)
 		
 		if directorios.present?
 		render json: directorios
